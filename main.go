7cdb8759fcb31ce2f88cc41af36ae18bcf4619ee
Qing Deng <bright2qing@gmail.com>
2026-07-23 08:01:24 -0400

    done

--- main.go
+++ main.go
@@ -5,42 +5,30 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
-type Stack struct {
-	items []int
+type Logger struct{}
+
+func (l Logger) Log(msg string) {
+	fmt.Printf("[log] %s\n", msg)
 }
 
-func (s *Stack) Push(x int) {
-	s.items = append(s.items, x)
+type Counter struct {
+	Logger
+	count int
 }
-func (s *Stack) Pop() (int, bool) {
-	if len(s.items) == 0 {
-		return 0, false
-	}
-	lastItem := s.items[len(s.items)-1]
-	newItems := s.items[:len(s.items)-1]
-	s.items = newItems
-	return lastItem, true
+
+func (c *Counter) Inc() {
+	c.count++
+	c.Log(strconv.Itoa(c.count))
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	parts := strings.Fields(sc.Text())
-	var s Stack
-	for _, p := range parts {
-		n, _ := strconv.Atoi(p)
-		_ = n
-		s.Push(n)
+	n, _ := strconv.Atoi(sc.Text())
+	var c Counter
+	for i := 0; i < n; i++ {
+		c.Inc()
 	}
-	for {
-		x, ok := s.Pop()
-		if !ok {
-			break
-		}
-		fmt.Println(x)
-	}
-	_ = fmt.Print
 }
---
+++ receiver.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) Push(x int) {
+	s.items = append(s.items, x)
+}
+func (s *Stack) Pop() (int, bool) {
+	if len(s.items) == 0 {
+		return 0, false
+	}
+	lastItem := s.items[len(s.items)-1]
+	newItems := s.items[:len(s.items)-1]
+	s.items = newItems
+	return lastItem, true
+}
+
+func main_receiver() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	parts := strings.Fields(sc.Text())
+	var s Stack
+	for _, p := range parts {
+		n, _ := strconv.Atoi(p)
+		_ = n
+		s.Push(n)
+	}
+	for {
+		x, ok := s.Pop()
+		if !ok {
+			break
+		}
+		fmt.Println(x)
+	}
+	_ = fmt.Print
+}
