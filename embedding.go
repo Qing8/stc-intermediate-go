71de20ac7527715ef4bc2acd38816c5ff2f63a1e
Qing Deng <bright2qing@gmail.com>
2026-07-23 08:18:28 -0400

    done

---
+++ embedding.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Logger struct{}
+
+func (l Logger) Log(msg string) {
+	fmt.Printf("[log] %s\n", msg)
+}
+
+type Counter struct {
+	Logger
+	count int
+}
+
+func (c *Counter) Inc() {
+	c.count++
+	c.Log(strconv.Itoa(c.count))
+}
+
+func main_embedding() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	var c Counter
+	for i := 0; i < n; i++ {
+		c.Inc()
+	}
+}
